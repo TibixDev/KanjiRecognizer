73a8587266673ca3d26d34d842ca7e2035aff3d0
Fülöp Tibor <fuloptibi03@gmail.com>
2022-08-17 13:13:37 +0300

    [🖊] Added title, favico, author & copyright footer

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,29 @@ import KanjiBoard from './views/KanjiBoard.vue';
 </script>
 
 <template>
-  <KanjiBoard/>
+    <div style="min-height: 90vh;" class="flex flex-col justify-between">
+        <KanjiBoard />
+        <div class="p-5 flex items-center flex-col">
+            <p>Copyright (c) 2022  <a class="text-violet-400 font-bold" href="https://github.com/TibixDev">TibixDev</a></p>
+            <p>ML Model: <a class="text-violet-400 font-bold" href="https://github.com/ichisadashioko/kanji-recognition">ichisadashioko</a></p>
+            <p>Licensed under the MIT license</p>
+        </div>
+    </div>
 </template>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    margin-top: 60px;
 }
 
 body {
-  margin: 0;
-  padding: 0;
-  background-color: rgb(47, 47, 47);
-  color: white;
+    margin: 0;
+    padding: 0;
+    background-color: rgb(47, 47, 47);
+    color: white;
 }
 
 .square {
